c911637d2366b12e87b58932ceae588e0e851282
Chinmay-innovates <chinmaygope69@gmail.com>
2024-08-27 00:20:47 +0530

    add update & delete workspace feature

--- src/app/workspace/[Id]/preferences-modal.tsx
+++ src/app/workspace/[Id]/preferences-modal.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import { toast } from "sonner";
+import { TrashIcon } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
 	Dialog,
+	DialogClose,
 	DialogContent,
+	DialogFooter,
 	DialogHeader,
 	DialogTitle,
+	DialogTrigger,
 } from "@/components/ui/dialog";
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useRemoveWorkspace } from "@/features/auth/workspaces/api/use-remove-workspace ";
 import { useUpdateWorkspace } from "@/features/auth/workspaces/api/use-update-workspace ";
-import { TrashIcon } from "lucide-react";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface PreferencesModalProps {
 	open: boolean;
@@ -20,11 +30,51 @@ export const PreferencesModal = ({
 	open,
 	setOpen,
 }: PreferencesModalProps) => {
+	const workspaceId = useWorkspaceId();
+	const router = useRouter();
+
 	const [value, setValue] = useState(initialValue);
+	const [editOpen, setEditOpen] = useState(false);
+
 	const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
 		useUpdateWorkspace();
 	const { mutate: removeWorkspace, isPending: isremovingWorkspace } =
 		useRemoveWorkspace();
+
+	const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		updateWorkspace(
+			{
+				id: workspaceId,
+				name: value,
+			},
+			{
+				onSuccess: () => {
+					setEditOpen(false);
+					toast.success("Workspace updated");
+				},
+				onError: () => {
+					toast.error("Failed to update workspace");
+				},
+			}
+		);
+	};
+	const handleRemove = () => {
+		removeWorkspace(
+			{
+				id: workspaceId,
+			},
+			{
+				onSuccess: () => {
+					toast.success("Workspace removed");
+					router.replace("/");
+				},
+				onError: () => {
+					toast.error("Failed to remove workspace");
+				},
+			}
+		);
+	};
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogContent className="p-0 bg-gray-50 overflow-hidden">
@@ -32,17 +82,48 @@ export const PreferencesModal = ({
 					<DialogTitle>{value}</DialogTitle>
 				</DialogHeader>
 				<div className="px-4 pb-4 flex flex-col gap-y-2">
-					<div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-						<div className="flex items-center justify-between">
-							<p className="text-sm font-bold">Workspace name</p>
-							<p className="text-sm text-[#1274A3] hover:underline font-semibold">
-								Edit
-							</p>
-						</div>
-						<p className="text-sm">{value}</p>
-					</div>
+					<Dialog open={editOpen} onOpenChange={setEditOpen}>
+						<DialogTrigger asChild>
+							<div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+								<div className="flex items-center justify-between">
+									<p className="text-sm font-bold">Workspace name</p>
+									<p className="text-sm text-[#1274A3] hover:underline font-semibold">
+										Edit
+									</p>
+								</div>
+								<p className="text-sm">{value}</p>
+							</div>
+						</DialogTrigger>
+						<DialogContent>
+							<DialogHeader>
+								<DialogTitle>Rename this workspace</DialogTitle>
+							</DialogHeader>
+							<form className="space-y-4" onSubmit={handleEdit}>
+								<Input
+									value={value}
+									disabled={isUpdatingWorkspace}
+									onChange={(e) => setValue(e.target.value)}
+									required
+									autoFocus
+									minLength={3}
+									maxLength={80}
+									placeholder="Workspace name e.g. 'Work' , 'Personal' , 'Home'"
+								/>
+								<DialogFooter>
+									<DialogClose asChild>
+										<Button variant="outline" disabled={isUpdatingWorkspace}>
+											Cancel
+										</Button>
+									</DialogClose>
+									<Button disabled={isUpdatingWorkspace}>Save</Button>
+								</DialogFooter>
+							</form>
+						</DialogContent>
+					</Dialog>
+
 					<button
-						disabled={false}
+						onClick={handleRemove}
+						disabled={isremovingWorkspace}
 						className="flex rounded-lg items-center gap-x-2 px-5 py-4 bg-white border cursor-pointer hover:bg-gray-50 text-rose-600"
 					>
 						<TrashIcon className="size-4" />
